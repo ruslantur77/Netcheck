21901c7863388f20889f4e2f120250ecc9639e75
imbalet <zhmaevva@gmail.com>
2025-10-24 23:36:23 +0300

    feat: add project structure and config

---
+++ agent/src/netcheck_agent/config.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from uuid import UUID
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class LogLevels(str, Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+
+class Config(BaseSettings):
+    LOG_LEVEL: LogLevels
+    REGISTRATION_TOKEN: UUID
+    REGISTRATION_URL: str
+    REGION: str
+    GET_IP_API_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+_config_instance: Config | None = None
+
+
+def get_config() -> Config:
+    global _config_instance
+    if _config_instance is None:
+        _config_instance = Config()  # type: ignore
+    return _config_instance
